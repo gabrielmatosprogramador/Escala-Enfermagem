fe1d35837aac6694ad59fbf5b9e8812ca5c5f07f
gabrielmatosprogramador <gabrielmatosprogramador@gmail.com>
2025-08-31 12:41:33 -0400

    Commit Inicial

---
+++ src/main/java/io/github/gabrielmatosprogramador/escalaEnfermagem/Escala/Enfermagem/EscalaEnfermagemApplication.java
@@ -0,0 +1,13 @@
+package io.github.gabrielmatosprogramador.escalaEnfermagem.Escala.Enfermagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalaEnfermagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalaEnfermagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gabrielmatosprogramador/escalaEnfermagem/Escala/Enfermagem/EscalaEnfermagemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabrielmatosprogramador.escalaEnfermagem.Escala.Enfermagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscalaEnfermagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
