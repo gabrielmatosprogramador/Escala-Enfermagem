4d63cade86aa0f6347982ed2758f82e1080458d8
gabrielmatosprogramador <gabrielmatosprogramador@gmail.com>
2025-08-31 13:29:03 -0400

    Criada as entidades Sitio e Escala, que sera responsavel por associar os relacionamentos.

---
+++ src/main/java/io/github/gabrielmatosprogramador/escalaEnfermagem/Escala/Enfermagem/Entities/Sitio.java
@@ -0,0 +1,28 @@
+package io.github.gabrielmatosprogramador.escalaEnfermagem.Escala.Enfermagem.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sitio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    private Boolean ativo;
+
+}
